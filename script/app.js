3de187665dd5b3289e73c1a3d55070bac655c12c
melodyBee <abeerafatimarub@gmail.com>
2023-04-11 00:29:26 +0500

    Initialize project

---
+++ script/app.js
@@ -0,0 +1,11 @@
+// var zakat_percentage= 0.025;
+// var user_input=prompt("Enter your number","00");
+// var result= +user_input*zakat_percentage;
+// alert("your zakat value is "+ result);
+
+
+// 11-4-2023
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+alert("Correct!");
+}
