c0a4adde403954dff240896767340ad10bcdc7f7
melodyBee <abeerafatimarub@gmail.com>
2023-04-12 06:31:19 +0500

    completed task 2

--- script/app.js
+++ script/app.js
@@ -1,3 +1,5 @@
+
+// Task:1 Zakah calclation
 // var zakat_percentage= 0.025;
 // var user_input=prompt("Enter your number","00");
 // var result= +user_input*zakat_percentage;
@@ -5,6 +7,7 @@
 
 
 // 11-4-2023
+// Practice today's code
 // var x = prompt("Where does the Pope live?");
 // if (x === "Vatican") {
 // alert("Correct!");
@@ -17,7 +20,39 @@
 // alert("Correct! ");
 // }
 
+// Task:2 Fitrah calculation
+var familyMembers=prompt("Enter total number of family members");
+var Form_of_fitrah=prompt("Enter the number of which form you want:\n1.Wheat 250 rs\n2.Groats 450 rs\n3.Dates 2100 rs\n4.Rasins 2800 rs");
+ if(Form_of_fitrah==1){
+    var w= 250*familyMembers;
+    alert("Fitrah amount this year is="+w);
+ }
+ else if(Form_of_fitrah==2)
+ {
+    var x= 450*familyMembers;
+    alert("Fitrah amount this year is=" + x);
+ }
+ else if(Form_of_fitrah==3)
+ {
+    var y= 2100*familyMembers;
+    alert=("Fitrah amount this year is="+y);
+ }
+else if(Form_of_fitrah==4)
+{
+var z= 2800*familyMembers;
+alert=("Fitrah amount this year is="+z);
+}
+else
+{
+    alert("wrong entry")
+}
 
+
+// Task:3 Secret number
+
+// Task:4 Capitilizing first letter
+// var name.toLowerCase()=prompt("Enter your name");
+// alert(name);
 
 //Creating a table
 // var j=parseInt(prompt("Type which number of the table you want"));
