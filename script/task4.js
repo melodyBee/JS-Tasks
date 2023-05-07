880d1c225fd1f2d66dab88f299f9db415fd154ff
melodyBee <abeerafatimarub@gmail.com>
2023-05-07 10:53:22 +0500

    yeah

---
+++ script/task4.js
@@ -0,0 +1,46 @@
+//ALERT
+// 1. Write a script to greet your website visitor using JS alert box.
+// alert("Welcome");
+
+//2. Write a script to display following message on your web page:
+// alert("Error! Please enter a valid password.");
+
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// VARIABLES FOR STRINGS
+
+//1. Declare a variable called username.
+// var username;
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName ="Abeera Fatima";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var message="Hello world";
+// alert(message);
+
+//4. Write a script to save student’s bio data in JS variables and show the data in alert boxes
+// var Name= prompt("Enter your name");
+// var age= prompt("Enter your age");
+// var qualification=  prompt("Enter your qualification");
+// alert(Name);
+// alert(age+" Years old");
+// alert(qualification);
+
+//5. Write a script to display the following alert using one JS variable:
+var x="pizza";
+for(j=0;j<5;j++){
+    for(i=0;i<5;i--){
+        console.log(j)
+    }
+}
+
+
